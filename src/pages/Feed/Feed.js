6dd670c8d2e0ee2fae5f6a6ff1fdc9ca015bca1d
David <davidakpughe2@gmail.com>
2020-03-07 18:05:47 +0100

    adding the header to all 'fetch'

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -50,7 +50,11 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/feed/posts?page=' + page)
+    fetch('http://localhost:8080/feed/posts?page=' + page, {
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch posts.');
@@ -126,9 +130,9 @@ class Feed extends Component {
       method: method,
       // mode: 'no-cors',
 
-      // headers:{
-      //   'Content-Type': 'application/json'
-      // },
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      },
       body: formData
     })
       .then(res => {
@@ -183,7 +187,10 @@ class Feed extends Component {
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
     fetch('http://localhost:8080/feed/post/' + postId, {
-      method: 'DELETE'
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
